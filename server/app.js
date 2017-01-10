5163a2f20e14d89df7fd4fff8019ce48d3560f68
imears <maxwellmears@gmail.com>
2017-01-10 17:40:04 -0500

    Don't show logs when testing

--- server/app.js
+++ server/app.js
@@ -3,14 +3,19 @@ const morgan  = require('morgan');
 const path    = require('path');
 
 const app = express();
+const env = process.env.NODE_ENV || 'development';
 
-// Setup logger
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+// Don't log during tests
+// Logging middleware
+if (env !== 'test') {
+  const logFormat = ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms';
+  app.use(morgan(logFormat));
+}
 
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-// Always return the main index.html, so react-router render the route in the client
+// Always return the main index.html, so react-router renders the route in the client
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
