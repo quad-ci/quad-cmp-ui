33103313ed5a320af5bf0211b4cdeebb483d4627
imears <maxwellmears@gmail.com>
2017-01-10 17:25:19 -0500

    [#137401797] Wire up axios for making calls to the server

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Router, Route } from 'react-router';
 
 import App from './components/App';
-import Members from './components/Members';
+import Users from './components/Users';
 import NotFound from './components/NotFound';
 
 const Routes = (props) => (
   <Router {...props}>
     <Route path="/" component={App} />
-    <Route path="/members" component={Members} />
+    <Route path="/users" component={Users} />
     <Route path="*" component={NotFound} />
   </Router>
 );
