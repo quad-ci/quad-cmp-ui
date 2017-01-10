e74415be8e0742b7c688c877ca56e5e0b7551629
imears <maxwellmears@gmail.com>
2017-01-10 17:37:27 -0500

    [#137375389] Connect MongoDB and Mongoose

--- server/index.js
+++ server/index.js
@@ -1,8 +1,33 @@
 'use strict';
 
-const app  = require('./app');
-const PORT = process.env.PORT || 9000;
+require('dotenv').config();
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+const app      = require('./app');
+const fs       = require('fs');
+const join     = require('path').join;
+const mongoose = require('mongoose');
+const config   = require('../config');
+
+const port   = process.env.PORT || 9000;
+const models = join(__dirname, 'models');
+
+// Bootstrap models
+fs.readdirSync(models)
+  .filter(file => file.search(/^[^\.].*\.js$/) !== -1)
+  .forEach(file => require(join(models, file)));
+
+connect()
+  .on('error', console.log)
+  .on('disconnected', connect)
+  .once('open', listen);
+
+function listen() {
+  if (app.get('env') === 'test') return;
+  app.listen(port);
+  console.log(`App listening on port ${port}`);
+}
+
+function connect() {
+  var options = { server: { socketOptions: { keepAlive: 1 } } };
+  return mongoose.connect(config.db, options).connection;
+}
