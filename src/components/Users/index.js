ad372347b460506870c475a439d308a74bd8bf2a
imears <maxwellmears@gmail.com>
2017-01-11 00:37:39 -0500

    [#137410395] Flash error message when users fail to load

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,30 +1,36 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import Snackbar from 'material-ui/Snackbar';
 
 import './style.css';
 
 export default class Users extends Component {
-  // static propTypes = {}
-  // static defaultProps = {}
-  // state = {}
 
   constructor(props) {
     super(props);
-    this.state = { users: [] };
+
+    this.state = {
+      message: '',
+      open: false,
+      users: []
+    };
   }
 
   componentDidMount() {
     this.loadUsers();
   }
 
+  handleRequestClose() {
+    this.setState({ open: false });
+  }
+
   loadUsers() {
-    axios.get('/v1/api/users').then(res => {
-      console.log('res', res);
-      this.setState({ users: res.users })
+    axios.get('/v1/api/users').then((res) => {
+      this.setState({ users: res.users });
     },
-    (err, status) => {
-      console.error(err, status);
+    (err) => {
+      this.setState({ message: String(err), open: true });
     });
   }
 
@@ -33,6 +39,7 @@ export default class Users extends Component {
     return (
       <div className={classnames('Users', className)} {...props}>
         <h1>Users</h1>
+        <Snackbar open={this.state.open} message={this.state.message} autoHideDuration={4000} onRequestClose={this.handleRequestClose} />
       </div>
     )
   }
