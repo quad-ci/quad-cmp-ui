9388bd3418b638209e01789c5df45752f144e63b
imears <maxwellmears@gmail.com>
2017-01-23 21:14:41 -0500

    Wire up AppBar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,45 @@
-import errorHelper from '../../utils/errorHelper.js';
-import React from 'react';
+import errorHelper from '../../utils/errorHelper';
+import React, { Component } from 'react';
 import styles from './App.css';
-import User from '../../model/User.js';
+import User from '../../model/User';
 
-export default class App extends React.Component {
+import { Link } from 'react-router';
+
+import logo from './quad-logo.png';
+
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+import DashboadIcon from 'material-ui/svg-icons/action/dashboard';
+import MenuIcon from 'material-ui/svg-icons/navigation/menu';
+import PowerIcon from 'material-ui/svg-icons/action/power-settings-new';
+import TimelineIcon from 'material-ui/svg-icons/action/timeline';
+import ViewListIcon from 'material-ui/svg-icons/action/view-list';
+
+class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      currentUser: {}
-    }
+      currentUser: []
+    };
+
+    this.style = {
+      button: {
+        textDecoration: 'none',
+        color: 'rgba(0, 0, 0, 0.870588)'
+      },
+      appBar: {
+        backgroundColor: '#0091EA'
+      },
+      quadLogo: {
+        height: '36px',
+        marginTop: '8px',
+        marginLeft: '40px'
+      }
+    };
   }
 
   componentDidMount() {
@@ -17,10 +48,25 @@ export default class App extends React.Component {
       .then( user => this.setState({ currentUser:user })  );
   }
 
+  menu() {
+    return (
+      <IconMenu iconButtonElement={<IconButton><MenuIcon /></IconButton>} targetOrigin={{horizontal: 'right', vertical: 'top'}} anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
+        <MenuItem primaryText={this.state.currentUser.username} leftIcon={<DashboadIcon />} containerElement={<Link to="/" style={this.style.button}></Link>}></MenuItem>
+        <MenuItem primaryText="Members" leftIcon={<ViewListIcon />} containerElement={<Link to="/members" style={this.style.button}></Link>}></MenuItem>
+        <MenuItem primaryText="Sign out" leftIcon={<PowerIcon />} containerElement={<Link to="/sign-out" style={this.style.button}></Link>}></MenuItem>
+      </IconMenu>
+    );
+  }
+
   render() {
-    return <div className={styles.app}>
-      <h2>Welcome to the new React platform</h2>
-      <h3>Current user: {this.state.currentUser.username}</h3>
-    </div>
+    return (
+      <div className={styles.app}>
+        <AppBar style={this.style.appBar} title="" iconElementLeft={<img src={logo} alt="logo" style={this.style.quadLogo}></img>} iconElementRight={this.menu()}/>
+        <h2>Welcome to the new React platform</h2>
+        <h3>Current user: {this.state.currentUser.username}</h3>
+      </div>
+    );
   }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import Routes from './routes';
 
+injectTapEventPlugin(); // Needed for onTouchTap - http://stackoverflow.com/a/34015469/988941
+
 ReactDOM.render(
-  <Routes history={browserHistory} />,
+  <MuiThemeProvider>
+    <Routes history={browserHistory} />
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
